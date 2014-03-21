bcb381f6c0ba2168d5b929f29d3cb74c545efda1
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-03-21 18:34:40 +0100

    Add script to refresh test boxes

---
+++ tests/refresh-boxes.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Refresh all boxes from what's made in the parent directory
+for series in precise trusty; do
+    for arch in i386 amd64; do
+        name=$series-desktop-$arch
+        if [ -f ../$name.box ]; then
+            echo "Reloading $name..."
+            vagrant box remove $name
+            vagrant box add $name ../$name.box
+        fi
+    done
+done
